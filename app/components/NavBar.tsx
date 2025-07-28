32c3a3fa76fdad01c22a4f8ffbd27c2697a913e1
Leonard <Oseghaleleonard39@gmail.com>
2025-07-28 19:45:54 +0100

    My styling just broke, i have no clue where the problem is even coming from

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,57 @@
 import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
+import { auth, signOut, signIn } from "@/auth"
 
-const Navbar = () => {
+const signInFunction = async () => {
+  "use server";
+  await signIn('github');
+}
+
+const signOutFunction = async () => {
+  "use server";
+  await signOut();
+}
+
+const Navbar = async () => {
+    const session = await auth();
+
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
         <nav className='flex justify-between items-center'>
             <Link href="/">
                 <Image src="/logo.png" alt="logo" width={144} height={30} />
             </Link>
+
+            <div className="flex items-center gap-5 text text-black">
+              {
+                session && session?.user ? (
+                  <>
+                    <Link href="/startup/create">
+                      <span>Create</span>
+                    </Link>
+
+                    <span>Signed in as {session?.user?.email}</span>
+
+                    <form action={signOutFunction}>
+                      <button>
+                        Log Out
+                      </button>
+                    </form>
+
+                    <Link href={`/user/$(session?.id)`}>
+                      {session?.user?.name}
+                    </Link>
+                  </>
+                ) : (
+                  <form action={signInFunction}>
+                    <button type="submit">
+                      Login
+                    </button>
+                  </form>
+                )
+              }
+            </div>
         </nav>
     </header>
   )
